0fa6bde5b8beeda73a8120ea5f75b1f97b619096
Olivier <cryptohouse1246@gmail.com>
2025-02-09 16:34:36 +0100

    On branch main modified:   Docker/docker-compose.yml modified:   Docker/init.sh modified:   Docker/prometheus.yml

--- Docker/init.sh
+++ Docker/init.sh
@@ -4,7 +4,7 @@ NODE_IP="255.255.255.255"
 MASSA_WALLET_ADDRESS="AU12...abcd"
 
 jsonExporterConfigPath="config.yml"
-prometheusConfigPath="prometheus.yml"
+# prometheusConfigPath="prometheus.yml"
 
 # jsonexporter config
 sed -i "s/YOUR_MASSA_ADDRESS/$MASSA_WALLET_ADDRESS/" "$jsonExporterConfigPath"
